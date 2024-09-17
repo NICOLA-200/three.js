171656fbf27e59917aa633e4fd931dee8ca1dc7f
Nicola <fatepepe66@gmail.com>
2024-09-17 18:35:24 +0200

    other changes

--- basic/main.js
+++ basic/main.js
@@ -1,53 +1,91 @@
-import * as THREE from 'three'
+// import * as THREE from 'three'
 
 
 
-// 1. create  a scene
+// // 1. create  a scene
 
-const scene =  new THREE.Scene();
-scene.background = new THREE.Color('#a0a0a0');
+// const scene =  new THREE.Scene();
+// scene.background = new THREE.Color('#a0a0a0');
 
-// 2. Add the camera
-const  camera =  new THREE.PerspectiveCamera(130, window.innerWidth  / window.innerHeight, 0.1, 500);
-camera.position.z = 2;
+// // 2. Add the camera
+// const  camera =  new THREE.PerspectiveCamera(130, window.innerWidth  / window.innerHeight, 0.1, 500);
+// camera.position.z = 2;
 
 
-//3. create and adda cube object 
-const  geometry = new THREE.BoxGeometry();
-const material =  new THREE.MeshLambertMaterial({ color: '#468585', emissive: '#468585' })
+// //3. create and adda cube object 
+// const  geometry = new THREE.BoxGeometry();
+// const material =  new THREE.MeshLambertMaterial({ color: '#468585', emissive: '#468585' })
 
  
 
-// 3 create  and add a cube  object
-const cube = new THREE.Mesh(geometry, material)
-scene.add(cube);
+// // 3 create  and add a cube  object
+// const cube = new THREE.Mesh(geometry, material)
+// scene.add(cube);
 
 
-// 4  add lighting
-const light  =  new THREE.DirectionalLight(0x9CDBA6, 10);
+// // 4  add lighting
+// const light  =  new THREE.DirectionalLight(0x9CDBA6, 10);
 
-light.position.set(1,3,5);
-scene.add(light);
-
-
-
-// 5 set up  the renderer
-const renderer =  new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
-
-// 6. Animate the scene
-
-function animate() {
-    requestAnimationFrame(animate);
-    cube.rotation.x += 0.03;
-    cube.rotation.y += 0.02;
+// light.position.set(1,3,5);
+// scene.add(light);
 
 
-    renderer.render(scene, camera); 
-}
 
-animate();
+// // 5 set up  the renderer
+// const renderer =  new THREE.WebGLRenderer();
+// renderer.setSize(window.innerWidth, window.innerHeight);
+// document.body.appendChild(renderer.domElement);
+
+// // 6. Animate the scene
+
+// function animate() {
+//     requestAnimationFrame(animate);
+//     cube.rotation.x += 0.03;
+//     cube.rotation.y += 0.02;
 
 
+//     renderer.render(scene, camera); 
+// }
+
+// animate();
+
+
+
+import * as THREE from "three"
+
+const w = innerWidth
+const h =  innerHeight
+
+
+
+// rendering
+const renderer = new THREE.WebGLRenderer();
+
+renderer.setSize(w,h)
+
+document.body.appendChild(renderer.domElement) 
+
+
+
+//camera
+const  camera =  new THREE.PerspectiveCamera(75, w / h, 0.1, 10)
+camera.position.z =  3;
+
+
+// scene 
+const scene =  new THREE.Scene()
+
+
+//object
+
+const goe = new THREE.IcosahedronGeometry(1.0, 2)
+const mat = new THREE.MeshBasicMaterial({
+    color: 0xccff,
+
+})
+
+const mesh  =  new THREE.Mesh(goe, mat )
+scene.add(mesh)
+
+renderer.render(scene, camera)
 
--- vite/counter.js
+++ vite/counter.js
@@ -1,4 +1,42 @@
 import * as THREE from 'three'
 
+
+const canvas = document.getElementById('canvas')
+// scene
 const  scene  =  new THREE.Scene();
-scene.background
+scene.background = new THREE.Color('#F0F0F0')
+
+
+//  camera
+
+const camera = new THREE.PerspectiveCamera(75, window.innerHeight /  window.innerWidth , 0.1 , 1000)
+camera.getWorldPosition.z = 5;
+
+// object
+
+const geometry =  new THREE.RingGeometry();
+const material  =  new  THREE.MeshDepthMaterial({color: '#339977',emissive: '#22ff44'})
+const item =  new THREE.Mesh(geometry, material);
+
+const boxGeo =   new THREE.BoxGeometry(2, 0.1 , 3);
+const boxMateria = new THREE.MeshBasicMaterial({color: "#aafdcd", emissive: '#aafdcd'})
+const  box =  new THREE.Mesh(boxGeo, boxMateria)
+box.position.y = -1.5;
+
+
+
+scene.add(item)
+scene.add(box)
+
+const light =  new THREE.SpotLight(0x006769,100)
+light.position.set(1,1,1);
+scene.add(light)
+
+
+//renderer
+
+const renderer  =  new THREE.WebGL3DRenderer({ canvas });
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+renderer.render(scene, camera);
